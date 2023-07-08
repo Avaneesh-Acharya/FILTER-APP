9cbff346d1fb026d6448e996e52d0b05bbec12b4
Avaneesh-Acharya <137638840+Avaneesh-Acharya@users.noreply.github.com>
2023-07-08 11:29:57 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-function preload() {
-    
+noseX=""
+noseY=""
+function preload() {    
+    clown_nose=loadImage("https://i.postimg.cc/MHXD6Cxb/bigotes-moustaches-black-cartoon-mustache-png-clipart-removebg-preview.png")
 }
 function setup() {
     canvas=createCanvas(890, 680)
@@ -15,13 +17,19 @@ function Got_Pose(Results) {
         console.log(Results)
         console.log("nose x="+Results[0].pose.nose.x)
         console.log("nose y="+Results[0].pose.nose.y)
-    }
+        noseX=Results[0].pose.nose.x
+        noseY=Results[0].pose.nose.y
+    }   
 }
 function Model_Loaded() {
     console.log("POSE NET IS INITIALIZED")
 }
 function draw() {
     image(video, 0, 0, 890, 680)
+    fill(255,0,0)
+    stroke(0,25, 37)
+    //circle(noseX, noseY, 15)//
+    image(clown_nose, noseX-75, noseY-10, 145,65)
 }
 function take_snapshot() {
     save("MYfilterimage.png")
